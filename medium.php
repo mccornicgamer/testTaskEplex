f04f2eb3bba68d079ba2822073aef07cceb0474d
Maxim Korchagin <mccornic@x-cart.com>
2025-09-25 00:39:29 +0400

    Medium task

---
+++ medium.php
@@ -0,0 +1,113 @@
+<?php
+//  [id, count, price, pack]
+$priceList = [
+    [111, 42, 13, 1],
+    [222, 77, 11, 10],
+    [333, 103, 10, 50],
+    [444, 65, 12, 5],
+];
+$priceList = [
+    [111,42,9,1],
+    [222,77,11,10],
+    [333,103,10,50],
+    [444,65,12,5],
+];
+$priceList = [
+    [111, 100, 30, 1],
+    [222, 60, 11, 10],
+    [333, 100, 13, 50],
+];
+
+// эта переменная содержит желаемое количество товаров для закупки
+$amountTarget = 76;
+
+function printPriceList($priceList) {
+    echo "Price list:\n";
+    echo "[id, count, price, pack]\n";
+    foreach ($priceList as $data) {
+        echo json_encode($data) . "\n";
+    }
+    echo "\n";
+}
+function printResult($result) {
+    echo "Order plan:\n";
+    $plan = [];
+    foreach ($result as $id => $data) {
+        $plan[] = [$id, $data[0]];
+    }
+    echo json_encode($plan) . "\n";
+}
+
+
+// сюда сохраним найденные "планы" закупок,
+// элемент имеет форматв формате [[id1 => [amount11, price1]], [id2 => amount22, price2] ...]
+$result = [];
+$minPrice = null;
+
+// будем использовать рекурсию для поиска вариантов,
+// эта функция обхода прайса и сбора "планов" для закупок, без учета общей стоимости
+//
+// $priceList - двумерный массив прайса
+// $amountTarget - необходимый кол-во товара на этой интерации обхода
+// $start - строка в списке прайса, с которой начинаем обход
+// $path - сюда собираем план закупки в формате [[id1 => amount11], [id2 => amount22] ...]
+// $result - сюда запиши окончатеьные варианты "палнов" закупок
+function findCombinations($priceList, $amountTarget, $start, $path, &$result, &$minPrice) {
+    // если в текущей итерации необходимое кол-во для заказа товара 0,
+    // то $path содержит заверщенный план закупки
+    if ($amountTarget == 0) {
+        $sum = 0;
+        foreach ($path as $id => $data) {
+            $sum += $data[0] * $data[1];
+        }
+
+        if (empty($minPrice) || $minPrice > $sum) {
+            $result = $path; // нашли комбинацию
+            $minPrice = $sum;
+        }
+
+        return;
+    }
+
+    for ($i = $start; $i < count($priceList); $i++) {
+        list($id, $count, $price, $pack) = $priceList[$i];
+        // если кол-во товаров в пакете больше количества товаров на складе,
+        // тогда не можем продать товар
+        if ($pack > $count) {
+            continue; // нет в наличии
+        }
+
+        if ($pack > $amountTarget) {
+            continue; // предложение превышает спрос, не будем анализировать 
+        }
+
+        // определим сколько пакетов товара может продать этот поставщшик
+        $packAmount = floor($count / $pack);
+
+        $amount = 0;
+        // начнем перебирать возможный заказ пактов от максимального кол-во пакетов
+        for ($p = $packAmount; $p >= 1; $p--) {
+            $amount = $p * $pack;
+            if ($amount > $amountTarget) {
+                continue; // превышаем целевое кол-во
+            }
+            // используем пакеты с общим кол-вом $amount
+            // и подберем комбинации из прайсов следующих поставщиков
+            findCombinations(
+                $priceList,
+                $amountTarget - $amount,
+                $i + 1,
+                $path + [$id => [$amount, $price]],
+                $result,
+                $minPrice
+            );
+        }
+    }
+}
+
+printPriceList($priceList);
+
+// Найдем все комбинации возможных "планов"
+findCombinations($priceList, $amountTarget, 0, [], $result, $minPrice);
+
+printResult($result);
